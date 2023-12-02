61ba2558551579bff95e8fff78b30fc2bf7399e9
Fareed Bakhsh <fareedbakhsh373@gmail.com>
2023-12-02 18:18:22 +0500

    first commit

---
+++ weeklyquestion.js
@@ -0,0 +1,93 @@
+//Exercise 5:  Write a program that determines if a person is eligible to vote based on their age.
+let age = 30;
+if(age >=18){
+    console.log('you are eligible for vote.')
+}else{
+    console.log('you are not eligible for vote.')
+};
+// simmilarly
+let age1 = 40;
+  if(age1<=18){
+    console.log('you are  eligible for vote.')
+  }else{
+    console.log('you are not eligible for vote.')
+  };
+ 
+
+//Exercise 4:   Write a program that checks if a given number is even or odd.
+
+
+let num = 10;
+if (num % 2===0){
+  console.log("Given num is even")
+} else{
+  console.log("Given num odd")
+};
+// will be odd
+
+let num1 = 10;
+if (num1 % 2 !== 0){
+  console.log("Given num is even")
+} else{
+  console.log("Given num is odd")
+};
+
+//
+//Exercise 6:  Write a program that calculates the result of a mathematical expression.	((10 + 5) * 3 - 2) / (4 % 3)  - 7)
+
+let 
+
+
+     result = ((10 + 5) * 3 - 2) / (4 % 3)  - 7
+// The mathimatical expression will be:
+     console.log( "The Mathimatical Expression :"  ,  result);
+
+
+//Exercise 3:  Write a program that checks if a given number is positive, negative, or zero.
+
+let  
+   num2 = 0;
+    
+   if (num2>0){
+    console.log("The given number is: ", +ive);
+   } else if (num2<0){
+   console.log("The given number is :" , -ive)
+   }else{
+    console.log("The given number is :", 0)};
+
+///Exercise 1:  Write a program that calculates the area of a rectangle.
+let rectangle;
+         
+        width = 9;
+      length = 15;
+                  solution = "area of rectangle =" ;length * width  ;
+                                         console.log("area of rectangle = " ,length * width ) ;      
+
+///Exercise 2: Write a program that takes input and calculates the volume of a cube.
+
+// By taking the a cube for finding  its volume
+let   l = 6;
+      h = 5;
+      w = 4;
+      VolumeOfCube = l*h*w;
+      console.log(" VolumeOfCube=", l * h * w);
+
+
+
+
+
+      
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
